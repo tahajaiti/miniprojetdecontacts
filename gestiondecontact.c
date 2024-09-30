ca1ed4bb2caaace2d9a8d11d329b8d651d452023
Taha Jaiti <kyojin.rar@gmail.com>
2024-09-30 18:36:15 +0100

    c

--- gestiondecontact.c
+++ gestiondecontact.c
@@ -3,6 +3,31 @@
 #include <ctype.h>
 #include <stdlib.h>
 
+#define MAX 100
+#define MAX_NAME 50
+#define MAX_NB 11
+#define MAX_EMAIL 50
+
+typedef struct {char nom[MAX_NAME];char num[MAX_NB];char email[MAX_EMAIL];}contacts;
+
+contacts contact[MAX];
+int contactcount = 0;
+
+int getchoice(){
+    int choice;
+    char input[7];
+
+    while (1) {
+        printf("Entrer une nombre (1-6): ");
+        fgets(input, sizeof(input), stdin);
+        if (sscanf(input, "%d", &choice) == 1 && choice >= 1 && choice <= 6) {
+            return choice;
+        }
+        printf("Erreur: Entrer un nombre valide entre 1 et 6.\n");
+    }
+
+}
+
 
 void menu(){
     printf("\n\t***** MENU *****\n");
@@ -16,9 +41,18 @@ void menu(){
 
 }
 
+
+
 int main(){
 
-    menu();
+    while (1) {
+        menu();
+        int choice = getchoice();
+        switch (choice) {
+            case 1: ; break;
+            
+        }
+    }
 
     return 0;
 }
