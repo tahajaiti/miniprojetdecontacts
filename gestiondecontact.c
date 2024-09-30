6802050eabaf0092fe34fadae8aa59d8e109ff32
Taha Jaiti <kyojin.rar@gmail.com>
2024-09-30 18:19:47 +0100

    gestiondecontact

---
+++ gestiondecontact.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+
+void menu(){
+    printf("\n\t***** MENU *****\n");
+    printf("1. Ajouter un Contact\n");
+    printf("2. Modifier un Contact\n");
+    printf("3. Supprimer un Contact\n");
+    printf("4. Afficher Tous les Contacts\n");
+    printf("5. Rechercher un Contact\n");
+    printf("6.Quitter\n");
+    printf("Entrer un nombre: ");
+
+}
+
+int main(){
+
+    menu();
+
+    return 0;
+}
