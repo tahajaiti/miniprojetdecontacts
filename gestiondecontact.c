6af55123a701ed99d4587419e46859fc2afa14d8
Taha Jaiti <kyojin.rar@gmail.com>
2024-09-30 19:22:52 +0100

    code

--- gestiondecontact.c
+++ gestiondecontact.c
@@ -13,22 +13,6 @@ typedef struct {char nom[MAX_NAME];char num[MAX_NB];char email[MAX_EMAIL];}conta
 contacts contact[MAX];
 int contactcount = 0;
 
-int getchoice(){
-    int choice;
-    char input[7];
-
-    while (1) {
-        printf("Entrer une nombre (1-6): ");
-        fgets(input, sizeof(input), stdin);
-        if (sscanf(input, "%d", &choice) == 1 && choice >= 1 && choice <= 6) {
-            return choice;
-        }
-        printf("Erreur: Entrer un nombre valide entre 1 et 6.\n");
-    }
-
-}
-
-
 void menu(){
     printf("\n\t***** MENU *****\n");
     printf("1. Ajouter un Contact\n");
@@ -40,6 +24,66 @@ void menu(){
     printf("Entrer un nombre: ");
 
 }
+int checkname(char *nom) {
+    for (int i = 0; nom[i] != '\0'; i++) {
+        if (!isalpha(nom[i]) && !isspace(nom[i])) {
+            printf("Erreur, Entrer un nombre valide.\n");
+            return 0;}}
+    return 1;
+}
+int checknum(char *num) {
+    return strlen(num) == 10 && 
+    (strncmp(num, "05", 2) == 0 ||
+    strncmp(num, "06", 2) == 0 ||
+    strncmp(num, "07", 2) == 0) &&
+    strspn(num, "0123456789") == 10;
+}
+
+int getchoice(){
+    int choice;
+    char input[10];
+
+    while (1) {
+        printf("Entrer une nombre (1-6): ");
+        fgets(input, sizeof(input), stdin);
+        if (sscanf(input, "%d", &choice) == 1) {
+            if (input[0] != '\n' && (input[1] == '\n' || input[1] == '\0')) {
+                if (choice >= 1 && choice <= 6) {
+                    return choice;}
+            }
+        }
+        printf("Erreur, Entrer un nombre (1-6).\n");}
+}
+void add(){
+    if (contactcount >= MAX){
+        printf("Le carnet est plein.\n");
+        return;}
+
+    contacts new;
+
+    do{
+        printf("Entrez le Nom de cette contact: ");
+        fgets(new.nom, sizeof(new.nom), stdin);
+        new.nom[strcspn(new.nom, "\n")] = 0;
+    } while (!checkname(new.nom));
+    
+    do{
+        printf("Entrez le Numero de contact: ");
+        fgets(new.num, sizeof(new.num), stdin);
+        new.num[strcspn(new.num, "\n")] = 0;
+    } while (!checknum(new.num));
+    
+    
+
+    printf("Entrez le Email de contact: ");
+    fgets(new.email, sizeof(new.email), stdin);
+    new.email[strcspn(new.email, "\n")] = 0;
+
+    contact[contactcount++] = new;
+    printf("Cet operation est succes!");
+}
+
+
 
 
 
@@ -49,7 +93,7 @@ int main(){
         menu();
         int choice = getchoice();
         switch (choice) {
-            case 1: ; break;
+            case 1:add(); break;
             
         }
     }
